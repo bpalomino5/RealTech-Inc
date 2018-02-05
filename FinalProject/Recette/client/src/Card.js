33d199aa34f09048a2c73c7dffba4e0dff0be245
Brandon Palomino <12bpalomino@gmail.com>
2018-02-05 04:03:27 -0800

    added semantic react libs and sample

--- FinalProject/Recette/client/src/Card.js
+++ FinalProject/Recette/client/src/Card.js
@@ -31,7 +31,7 @@ class CardBody extends Component {
   render() {
     return (
       <div className="card-body">
-        <h2>{this.props.title}</h2>       
+        <h2 className="card-body--title">{this.props.title}</h2>       
         <p className="body-content">{this.props.text}</p>        
       </div>
     )
