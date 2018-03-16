9ad1c015b5ad0c295bd5caf0f60d50536e83ec6c
Brandon Palomino <12bpalomino@gmail.com>
2018-03-15 19:39:48 -0700

    changed to hashrouting

--- Project-R/Recette/client/src/index.js
+++ Project-R/Recette/client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './layouts/index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>, document.getElementById('root'));
+	</HashRouter>, document.getElementById('root'));
